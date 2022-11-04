c3ccd1711aaef52445b55416ea00f119163e452c
cartiniv <105551264+cartiniv@users.noreply.github.com>
2022-11-04 06:59:02 -0300

    update CF

--- src/main/java/com/mycompany/aulabancodados/ConnectionFactory.java
+++ src/main/java/com/mycompany/aulabancodados/ConnectionFactory.java
@@ -4,9 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
+//classe especifica para a conexão com o banco de dados, nessa classe definimos a String de conexão com o banco de dados, usando a biblioteca DriverManager do MySQL
 public class ConnectionFactory {
     private String usuario = "root";
-    private String senha = "Alanis01";
+    private String senha = "senha";
     private String host = "localhost";
     private String porta = "3306";
     private String bd = "db_pessoas";
